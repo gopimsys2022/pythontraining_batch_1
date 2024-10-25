dc157b7474a936dd0948303d904f060492013bfe
Kamesh269 <kamesh2983@gmail.com>
2024-10-25 17:45:02 +0530

    Update sample.py

--- kamesh/sample.py
+++ kamesh/sample.py
@@ -1,1 +1,38 @@
- 
+#Exercise: Level 1
+
+firstname = "python"
+lastname = "programming"
+fullname = "python programming"
+country = "India"
+city = "Hyderabad"
+age = "25"
+year = "2024"
+is_married = "No"
+is_true = "True" 
+is_light_on = "No"
+a,b = 3,7
+
+print(firstname)
+print(lastname)
+print(fullname)
+print(country)
+print(city)
+print(age)
+print(year)
+print(is_married)
+print(is_true)
+print(is_light_on)
+print(a,b)
+
+Output :
+python
+programming
+python programming
+India
+Hyderabad
+25
+2024
+No
+True
+No
+3 7
