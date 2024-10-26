3a082c3bf5a33039b3b983b1d264663bffda5d7c
mmanideep <50361654+maddalimanideep@users.noreply.github.com>
2024-10-26 17:10:05 +0530

    Update sample.py

--- manideep/sample.py
+++ manideep/sample.py
@@ -1,1 +1,62 @@
+Operators :
+
+Addition
+x = 5
+y = 10
+print(x + y)
+o/p:
+15
+
+passing values as User input
+x = input("Enter x value: ")
+y = input("Enter y value: ")
+
+sum = int(x) + int(y)
+
+print("The sum is: ", sum)
+o/p:
+Enter x value: 10
+Enter y value: 15
+The sum is: 25
+
+Subtraction:
+v1 = 6
+v2 = 5
+res = v1 - v2
+print(res)
+o/p:
+1
+
+#multiplication operator:
+v3 = 2
+v4 = 3
+mres = v3 * v4
+print(mres)
+o/p:
+6
+
+# division operator
+v5 = 6
+v6 = 2
+dres = v5 / v6
+print(dres)
+o/p:
+3.0
+
+# floor division
+v7 = 7
+v8 = 6
+fres = v7 // v8
+print(fres)
+o/p:
+1
+
+# modulus operator
+v9 = 13
+v10 = 11
+mres = v9 % v10
+print(mres)
+o/p:
+2
+
 
