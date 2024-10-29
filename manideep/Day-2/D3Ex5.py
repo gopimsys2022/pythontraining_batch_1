9ab75a7fc6a82fd34393e2c23dea3e1a2fe9428b
manideepmaddali <manideepmaddali@gmail.com>
2024-10-29 13:47:57 +0530

    program created

---
+++ manideep/Day-2/D3Ex5.py
@@ -0,0 +1,34 @@
+# Declare an empty list
+list = []
+print (list)
+# Declare a list with more than 5 items
+list1=["mani","chaitu","kamesh","veera","gopi"]
+print (list1)
+# Find the length of your list
+list2 = len(list1)
+print(list2)
+
+# Get the first item, the middle item and the last item of the list
+first_item=list1[0]
+print (first_item)
+middle_item=list1[2]
+print (middle_item)
+last_item=list1[-1]
+print (last_item)
+
+# Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_type =["manideep", 30 , 5.10, "single", "perumbakkam", "chennai"]
+print (mixed_data_type)
+
+#Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+it_companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+print (it_companies)
+
+# Print the list using print()
+lst =["name","age"]
+print (lst)
+
+# Print the number of companies in the list
+companies = ["flipkart", "amazonn", "netflix"]
+cl=len(companies)
+print (cl)
