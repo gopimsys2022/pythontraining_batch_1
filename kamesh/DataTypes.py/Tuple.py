34e84c1fa8a1fab61b24f344c1c376660b162a2d
kamesh k <kamesh2983@gmail.com>
2024-10-30 11:36:07 +0530

    fileadded

---
+++ kamesh/DataTypes.py/Tuple.py
@@ -0,0 +1,16 @@
+#Empty Tuple
+tuple =()
+print(tuple)
+
+#Containing names
+brothers = ("kiran","ajay","vikas")
+sisters = ("sri","kumari")
+siblings = brothers + sisters
+print(siblings)
+
+#length
+print(len(siblings))
+
+#Modify
+#siblings = 
+
