416338f0bcbef951d4cd18da10512919a800eca4
manideepmaddali <manideepmaddali@gmail.com>
2024-10-30 15:53:39 +0530

    Tuple completed

--- manideep/Day-2/pra.py
+++ manideep/Day-2/pra.py
@@ -1,2 +1,18 @@
+fruits = ('apple', 'banana', 'orange', 'grape', 'kiwi')
+vegetables = ('carrot', 'broccoli', 'spinach', 'pepper', 'potato')
+animal_products = ('chicken', 'beef', 'milk', 'cheese', 'egg')
 
+# Join the three tuples into one tuple
+food_stuff_tp = fruits + vegetables + animal_products
 
+# Change the food_stuff_tp tuple to a food_stuff_lt list
+food_stuff_lt = list(food_stuff_tp)
+
+print (food_stuff_lt)
+
+# Slice out the first three items and the last three items
+ft = food_stuff_lt[:3]
+print (ft)
+
+lt = food_stuff_lt[-3:]
+print (lt)
