55cff0b7a20cb3d4bc440595b0d6b07d43135fe5
kamesh k <kamesh2983@gmail.com>
2024-10-30 18:42:40 +0530

    FinalList

--- kamesh/DataTypes.py/List.py
+++ kamesh/DataTypes.py/List.py
@@ -223,3 +223,54 @@ print(first_country)
 print(second_country)
 print(third_country)
 print(scandic_countries)
+
+output
+
+[]
+['Apple', 'Mango', 'Kiwi', 'Banana', 'Orange', 'Sapota']
+6
+Apple
+3
+Sapota
+['Pavan', 28, 5.8, 'Single', '3-34 kiran nagar']
+['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+7
+Facebook
+3
+Amazon
+['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon', 'TCS']
+['Facebook', 'Google', 'Microsoft', 'Apple', 'Capgemini', 'IBM', 'Oracle', 'Amazon']
+MICROSOFT
+Facebook # Google # Microsoft # Apple # IBM # Oracle # Amazon
+True
+None
+None
+['Facebook', 'Google', 'Microsoft']
+['IBM', 'Oracle', 'Amazon']
+Apple
+Facebook
+Apple
+Amazon
+None
+['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'Express', 'MongoDB']
+['HTML', 'CSS', 'JS', 'React', 'Redux', 'Python', 'SQl', 'Node', 'Express', 'MongoDB']
+[19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+[19, 19, 20, 22, 24, 24, 24, 25, 25, 26]
+26
+19
+[19, 22, 19, 24, 20, 25, 26, 24, 25, 24, 26]
+[19, 22, 19, 24, 20, 25, 26, 24, 25, 24, 19]
+[19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+10
+228
+22.8
+7
+Lesotho
+['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo (Brazzaville)', 'Congo', 'Costa Rica', "Cote d'Ivoire", 'Croatia', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor (Timor Timur)', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 
+'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia, The', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Korea, North', 'Korea, South', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho']
+['Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia and Montenegro', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United 
+States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe']        
+China
+Russia
+USA
+['Finland', 'Sweden', 'Norway', 'Denmark']
