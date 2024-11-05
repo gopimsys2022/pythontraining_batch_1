e0d698be4d6f721b73d39400b79f2312e874e7cf
manideepmaddali <manideepmaddali@gmail.com>
2024-11-05 12:06:01 +0530

    Arranged all files as structured

--- manideep/pra.py
+++ manideep/pra.py
@@ -1,23 +1,23 @@
-person = {
-    'first_name': 'Asabeneh',
-    'last_name': 'Yetayeh',
-    'age': 250,
-    'country': 'Finland',
-    'is_married': True,  # Fixed the key spelling
-    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'address': {
-        'street': 'Space street',
-        'zipcode': '02210'
-    }
-}
+# person = {
+#     'first_name': 'Asabeneh',
+#     'last_name': 'Yetayeh',
+#     'age': 250,
+#     'country': 'Finland',
+#     'is_married': True,  # Fixed the key spelling
+#     'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+#     'address': {
+#         'street': 'Space street',
+#         'zipcode': '02210'
+#     }
+# }
 
-# Check if the person has the skills key
-if 'skills' in person:
-    skills = person['skills']
+# # Check if the person has the skills key
+# if 'skills' in person:
+#     skills = person['skills']
     
-    # Print out the middle skill
-    middle_index = len(skills) // 2
-    middle_skill = skills[middle_index]
-    print(f"The middle skill is: {middle_skill}")
-else:
-    print("Unknown title.")
+#     # Print out the middle skill
+#     middle_index = len(skills) // 2
+#     middle_skill = skills[middle_index]
+#     print(f"The middle skill is: {middle_skill}")
+# else:
+#     print("Unknown title.")
