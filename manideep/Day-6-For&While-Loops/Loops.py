0ebb076aa031fb7f72c720feff5fa7a48ac43dfe
manideepmaddali <manideepmaddali@gmail.com>
2024-11-06 16:15:13 +0530

    added new excercise

--- manideep/Day-6-For&While-Loops/Loops.py
+++ manideep/Day-6-For&While-Loops/Loops.py
@@ -39,6 +39,8 @@ for i in range (1 , 8):
 # # # # # # # # #
 # # # # # # # # #
 # # # # # # # # #
+for i in range (8):
+    print ("#" *8)
 
 # Print the following pattern:
 # 0 x 0 = 0
@@ -65,6 +67,12 @@ for num in range(1, 101):
         print(f"{num} is even") 
     else: 
         print(f"{num} is odd") 
+#          (or)
+# for i in range(0, 101, 2):
+#     print(i)
+
+# for i in range(1, 101, 2):
+#     print(i)
 
 # Exercises: Level 2
 # Use for loop to iterate from 0 to 100 and print the sum of all numbers.
@@ -88,7 +96,46 @@ print(f"The sum of all evens is {even_total}. And the sum of all odds is {odd_to
 
 # Exercises: Level 3
 # Go to the data folder and use the countries.py file. Loop through the countries and extract all the countries containing the word land.
+countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 
+  'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 
+  'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 
+  'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 
+  'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 
+  'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 
+  'Congo (Brazzaville)', 'Congo', 'Costa Rica', "Cote d'Ivoire", 'Croatia', 'Cuba', 
+  'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 
+  'East Timor (Timor Timur)', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 
+  'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia, The', 
+  'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea', 
+  'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hungary', 'Iceland', 'India', 
+  'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 
+  'Kazakhstan', 'Kenya', 'Kiribati', 'Korea, North', 'Korea, South', 'Kuwait', 'Kyrgyzstan', 
+  'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 
+  'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 
+  'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Micronesia', 'Moldova', 'Monaco', 
+  'Mongolia', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 
+  'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 
+  'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 
+  'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent', 'Samoa', 'San Marino', 
+  'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia and Montenegro', 'Seychelles', 
+  'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 
+  'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 
+  'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 
+  'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 
+  'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Venezuela', 'Vietnam', 'Yemen', 
+  'Zambia', 'Zimbabwe',];
+for country in countries:
+    if 'land' in country:
+        print(country)   
+
+
 # This is a fruit list, ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop.
+ts = ['banana', 'orange', 'mango', 'lemon']
+rev_fruits =[ ]
+for i in fruits:
+    rev_fruits.insert (0 , i)
+print (rev_fruits)
+
 # Go to the data folder and use the countries_data.py file.
 # What are the total number of languages in the data
 # Find the ten most spoken languages from the data
--- manideep/pra.py
+++ manideep/pra.py
@@ -23,38 +23,8 @@
 #     print("Unknown title.")
 
 
-# Iterate 0 to 10 using for loop, do the same using while loop.
 
-for i in range(11):
-    print(i)
 
-#
-i = 0
-while i <= 10:
-    print(i)
-    i += 1
-
-# Iterate 10 to 0 using for loop, do the same using while loop.
-
-# Write a loop that makes seven calls to print(), so we get on the output the following triangle:
-
-#   #
-#   ##
-#   ###
-#   ####
-#   #####
-#   ######
-#   #######
-# Use nested loops to create the following:
-
-# # # # # # # # #
-# # # # # # # # #
-# # # # # # # # #
-# # # # # # # # #
-# # # # # # # # #
-# # # # # # # # #
-# # # # # # # # #
-# # # # # # # # #
 # Print the following pattern:
 
 # 0 x 0 = 0
@@ -68,18 +38,7 @@ while i <= 10:
 # 8 x 8 = 64
 # 9 x 9 = 81
 # 10 x 10 = 100
-# Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items.
 
-# Use for loop to iterate from 0 to 100 and print only even numbers
-
-# Use for loop to iterate from 0 to 100 and print only odd numbers
-
-# Exercises: Level 2
-# Use for loop to iterate from 0 to 100 and print the sum of all numbers.
-# The sum of all numbers is 5050.
-# Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
-
-# The sum of all evens is 2550. And the sum of all odds is 2500.
 # Exercises: Level 3
 # Go to the data folder and use the countries.py file. Loop through the countries and extract all the countries containing the word land.
 # This is a fruit list, ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop.
@@ -87,3 +46,4 @@ while i <= 10:
 # What are the total number of languages in the data
 # Find the ten most spoken languages from the data
 # Find the 10 most populated countries in the world
+# Go to the data folder and use the countries.py file. Loop through the countries and extract all the countries containing the word land.
