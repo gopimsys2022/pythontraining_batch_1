e3e3dcb042edcd4a14e62ceb066765159a993028
Veershetty Laxman <vlaxman@AD.MSYSTECHNOLOGIES.COM>
2024-11-13 12:34:27 +0530

    msdjkdjs

--- veershetty/practice/sample.py
+++ veershetty/practice/sample.py
@@ -15,3 +15,11 @@ Traceback (most recent call last):
   File "<stdin>", line 1, in <module>
 ModuleNotFoundError: No module named 'maths'
 >>>
+Python 3.9.6 (default, Jun 28 2021, 15:26:21)
+[Clang 11.0.0 (clang-1100.0.33.8)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import maths
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ModuleNotFoundError: No module named 'maths'
+>>>
