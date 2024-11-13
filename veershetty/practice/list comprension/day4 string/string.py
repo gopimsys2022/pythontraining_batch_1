fd31afb995f68a8691329a9fd7f6b0de09747b7e
Veershetty Laxman <vlaxman@AD.MSYSTECHNOLOGIES.COM>
2024-11-13 16:23:33 +0530

    list

--- veershetty/practice/list comprension/day4 string/string.py
+++ veershetty/practice/list comprension/day4 string/string.py
@@ -4,3 +4,9 @@ print(len(letter))
 greeting = 'Hello, World!'  
 print(greeting)             
 print(len(greeting))
+
+fruits = ['banana', 'apple', 'mango', 'onion'] 
+last_fruit = fruits[-1]
+second_last = fruits[-2]
+print(last_fruit)       
+print(second_last)
