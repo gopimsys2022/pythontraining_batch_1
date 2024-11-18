ce4b9aa5392746b4728abacbbcc99838385b8eab
Veershetty Laxman <vlaxman@AD.MSYSTECHNOLOGIES.COM>
2024-11-18 15:44:56 +0530

    module.py

--- veershetty/day12 modules.py
+++ veershetty/day12 modules.py
@@ -6,3 +6,11 @@ os.rmdir()
 
 import sys 
 print('Welcome {}. Enjoy  {} challenge!'.format(sys.argv[1], sys.argv[2]))
+
+import math
+print(math.pi)           
+print(math.sqrt(2))      
+print(math.pow(2, 3))    
+print(math.floor(9.81))  
+print(math.ceil(9.81))   
+print(math.log10(100)) 
