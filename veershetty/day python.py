8d8e269f3fffbbe9dd12d3ae6c683d1d1ad2e67f
Veershetty Laxman <vlaxman@AD.MSYSTECHNOLOGIES.COM>
2024-11-18 14:27:26 +0530

    da9 python

---
+++ veershetty/day python.py
@@ -0,0 +1,14 @@
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+person['first_name'] = 'Eyob'
+person['age'] = 252
