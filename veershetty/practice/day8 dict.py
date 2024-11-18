32c76d56ebf9b82277f0bfa6b686ec339793aa11
Veershetty Laxman <vlaxman@AD.MSYSTECHNOLOGIES.COM>
2024-11-18 14:20:27 +0530

    day8 practice

--- veershetty/practice/day8 dict.py
+++ veershetty/practice/day8 dict.py
@@ -10,4 +10,23 @@ for num in l:
     if l.count(num) ==1:
         print(num)
 
+        person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+print(person['first_name'])
+print(person['country'])    
+print(person['skills'])     
+print(person['skills'][0]) 
+print(person['address']['street'])
+print(person['city'])       
+
         
