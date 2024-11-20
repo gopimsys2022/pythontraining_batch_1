efb63b30c4249e45341206da166a4121464ca535
Veershetty Laxman <vlaxman@AD.MSYSTECHNOLOGIES.COM>
2024-11-20 08:07:13 +0530

    regex

---
+++ veershetty/regex day17.py
@@ -0,0 +1,12 @@
+re.match(substring, string, re.I)
+
+import re
+txt = 'I love to teach python and javaScript'
+match = re.match('I love to teach', txt, re.I)
+print(match) 
+span = match.span()
+print(span)    
+start, end = span
+print(start, end)
+substring = txt[start:end]
+print(substring)      
