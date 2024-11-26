159bfdc70395f09daf70a5b06b94c3aea765a034
Veershetty Laxman <vlaxman@AD.MSYSTECHNOLOGIES.COM>
2024-11-26 14:50:26 +0530

    excesories

---
+++ veershetty/list_excesories.py
@@ -0,0 +1,21 @@
+Declare an empty list
+my_list = []
+
+Declare a list with more than 5 items?
+my_list = [1, 2, 3, 4, 5, 6, 7]
+
+Find the length of your list?
+length_of_list = len(my_list)
+print(length_of_list)
+
+Get the first item, the middle item and the last item of the list?
+my_list = [1, 2, 3, 4, 5, 6, 7]
+first_item = my_list[0]
+middle_item = my_list[len(my_list) // 2]
+last_item = my_list[-1]
+print("First item:", first_item)
+print("Middle item:", middle_item)
+print("Last item:", last_item)
+
+Declare a list called mixed_data_types, put your(name, age, height, marital status, address)?
+mixed_data_types = ["John Doe", 30, 5.9, "Single", "123 Main St, City, Country"]
